fdef19b94560b9cfce12dae951b128d08ea499b8
Miguel Figueroa <miguelfigueroa90@gmail.com>
2021-01-29 13:36:26 -0400

    Adding initial config

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Note;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()->create([
+            'email' => 'miguelfigueroa90@gmail.com',
+            'password' => bcrypt('123456789'),
+        ]);
+        Note::factory(30)->create();
     }
 }
