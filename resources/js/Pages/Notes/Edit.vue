aa4042fab74a7726748dcfef9815bb9fdb255076
Miguel Figueroa <miguelfigueroa90@gmail.com>
2021-01-29 22:26:50 -0400

    Adding note edit form

---
+++ resources/js/Pages/Notes/Edit.vue
@@ -0,0 +1,68 @@
+<template>
+    <app-layout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Notes
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="md:grid md:grid-cols-3 md-gap-6">
+                    <div class="md:col-span-1">
+                        <div class="px-4 sm:px0">
+                            <h3 class="text-lg text-gray-900">Edit Note</h3>
+                            <p class="text-sm text-gray-600">If edit this note, you cant back to the previous state</p>
+                        </div>
+                    </div>
+                    <div class="md:col-span-2 mt-5 md:mt-0">
+                        <div class="shadow bg-white md:rounded-md p-4">
+                            <form>
+                                <label class="block font-medium text-sm text-gray-700">
+                                    Summary
+                                </label>
+                                <textarea
+                                    class="form-input w-full rounded-md shadow-sm"
+                                    v-model="form.excerpt">
+                                </textarea>
+                                <label class="block font-medium text-sm text-gray-700 mt-2">
+                                    Content
+                                </label>
+                                <textarea
+                                    class="form-input w-full rounded-md shadow-sm"
+                                    v-model="form.content"
+                                    rows="8">
+                                </textarea>
+                                <button
+                                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md mt-2">
+                                    Edit
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </app-layout>
+</template>
+
+<script>
+    import AppLayout from '@/Layouts/AppLayout'
+
+    export default {
+        components: {
+            AppLayout,
+        },
+        props: {
+            note: Object,
+        },
+        data() {
+            return {
+                form: {
+                    excerpt: this.note.excerpt,
+                    content: this.note.content,
+                }
+            }
+        }
+    }
+</script>
